609cc9734d3f3fa428c7695f911947cc1a447d86
Frank Vega <vega.frank@gmail.com>
2026-08-26 10:26:15 -0400

    Version v0.0.6

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from pathlib import Path
 
 import setuptools
 
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 NAME = "esperanza"
 
 INSTALL_REQUIRES = [
-    "numpy>=2.2.1",
-    "scipy>=1.15.0",
-    "networkx[default]>=3.4.2"
+    "hvala>=0.1.2"
 ]
 
 setuptools.setup(
